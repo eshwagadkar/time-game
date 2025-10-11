20564fe8cf88d462318ab29d93f14a9c4d1ed176
eshwagadkar <eshwagadkar007@gmail.com>
2025-10-11 11:36:09 +0530

    just a alternative forwardRef approach for forwarding refs prior to react v.19

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,23 +1,25 @@
-import { useImperativeHandle, useRef } from "react"
+import { useImperativeHandle, useRef, forwardRef } from "react"
 
-export default function ResultModal({ref, result, targetTime}) {
-
-    const dialog = useRef()
-
-    useImperativeHandle(ref, () => {
-        return {
-            open() {
-                dialog.current.showModal()
+const ResultModal = forwardRef(function Modal({result, targetTime}, ref) {
+    
+        const dialog = useRef()
+    
+        useImperativeHandle(ref, () => {
+            return {
+                open() {
+                    dialog.current.showModal()
+                }
             }
-        }
-    })
+        })
+    
+        return <dialog ref={dialog} className="result-modal">
+            <h2>You {result}</h2>
+            <p>The target time was <strong>{targetTime}</strong></p>
+            <p>You stopped the timer with X seconds left.</p>
+            <form method="dialog">
+                <button>Close</button>
+            </form>
+        </dialog>
+    }) 
 
-    return <dialog ref={dialog} className="result-modal">
-        <h2>Your {result}</h2>
-        <p>The target time was <strong>{targetTime}</strong></p>
-        <p>You stopped the timer with X seconds left.</p>
-        <form method="dialog">
-            <button>Close</button>
-        </form>
-    </dialog>
-}
+export default ResultModal
